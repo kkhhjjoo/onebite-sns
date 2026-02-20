cf1f676540aefc1a26846f50ed44ea0ee610ab5e
kkhhjjoo <khjoo1203@gmail.com>
2026-02-20 21:56:45 +0900

    포스트 추가 기능 구현하기

--- src/pages/sign-up-page.tsx
+++ src/pages/sign-up-page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useSignUp } from "@/hooks/mutations/user-sign-up";
+import { useSignUp } from "@/hooks/mutations/auth/user-sign-up";
 import { generateErrorMessage } from "@/lib/error";
 import { useState } from "react";
 import { Link } from "react-router";
